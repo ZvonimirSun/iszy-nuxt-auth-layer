fae4e08b65835e305b1d4de9c2faece19d2ba567
ZvonimirSun <hi@iszy.xyz>
2026-07-18 13:47:26 +0800

    refactor: 移除冗余认证功能代理

--- server/api/auth/features.get.ts
+++
@@ -1,6 +0,0 @@
-import type { ResultDto } from '@zvonimirsun/iszy-common'
-import type { AuthFeatures } from '##shared/types/auth'
-
-export default defineEventHandler(async (event): Promise<ResultDto<AuthFeatures>> => {
-  return await authFetch(event)<ResultDto<AuthFeatures>>('/auth/features')
-})
